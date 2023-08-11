e90c6053d26ec1cd1e397b82e8951ec2bb20aa4c
26luiz0202 <141413993+26luiz0202@users.noreply.github.com>
2023-08-11 08:18:27 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(500, 500);
+background("red");
+}
+  
+function draw() {
+  
+  fill("black");
+  
+ stroke("purple") 
+ 
+ if(mouseIsPressed){}
+  rect(mouseX,mouseY,30,30);
+}
